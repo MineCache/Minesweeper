680ecadbfe85008686ceed55645645ada26bec87
l-tn3314 <l.tn3314@gmail.com>
2014-12-22 11:44:53 -0500

    redirect links working

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ app.secret_key = 'dont_tell'
 @app.route("/", methods = ['GET','POST'])
 @app.route("/home", methods = ['GET','POST'])
 def index():
-    if request.method == 'GET':
+    #if request.method == 'GET':
         return render_template("home.html")
-    else:
-        return redirect(url_for('about'))
+    #else:
+    #    return redirect(url_for('about'))
 
 @app.route("/about", methods = ['GET','POST'])
 def about():
-    if request.method == 'GET':
+    #if request.method == 'GET':
         return render_template("about.html")
-    else:
-        return redirect(url_for('home'))
+    #else:
+    #    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.debug = True
