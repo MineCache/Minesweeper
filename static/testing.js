e4770b0df7d8ed732c7bf887591eac7308ad3338
l-tn3314 <l.tn3314@gmail.com>
2015-01-12 11:46:24 -0500

    mouse tracking

--- static/testing.js
+++ static/testing.js
@@ -1,8 +1,6 @@
-var mouseX;
-var mouseY;
-var width;
-var height;
-
+var mouseX, mouseY;
+var width, height;
+var gamebox = document.getElementById("game");
 
 
 
@@ -14,7 +12,14 @@ document.getElementById("pause").addEventListener('click',pause);
 document.getElementById("resume").addEventListener('click',resume);
 
 window.addEventListener('mousemove',function(e){
-    mouseX = e.pageX;
-    //console.log(mouseX);
-    mouseY = e.pageY;
+    mouseX = e.pageX - gamebox.offsetLeft;
+    if (mouseX >= 0 && mouseX <=1200){
+	//function to check relative mine location
+	//console.log(mouseX);
+    }
+    mouseY = e.pageY - gamebox.offsetTop;
+    if (mouseY >=0 && mouseY <=600){
+	//function to check relative mine location
+	//console.log(mouseY);
+    }
 });
