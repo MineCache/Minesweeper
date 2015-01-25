afd345a1beb3043de079afad69f977dbd281a267
Applesauce136 <harrison.chiu.136@gmail.com>
2015-01-25 00:15:43 -0500

    database fully integrated! basically!

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, redirect, request, url_for, flash, session
+import math
 
 import mine_sqlite3
 
 app = Flask(__name__)
 app.secret_key = 'dont_tell'
 
-@app.route("/home", methods = ['GET','POST'])
+@app.route("/", methods = ['GET','POST'])
+@app.route("/index", methods = ['GET','POST'])
 def home():
-    if request.method == 'GET':
+    if 'user' in session:
         return render_template("home.html")
     else:
-        return redirect(url_for('about'))
+        return redirect(url_for('login'))
 
 
 @app.route("/about", methods = ['GET','POST'])
@@ -23,16 +25,22 @@ def test():
         if "user" in session:
             db = mine_sqlite3.init()
             mine_sqlite3.create(db)
-            incUser(db, session["user"], request.form["points"])
+            #same number of characters????
+            print "~~~~ *" + session["user"][1] + ": " + request.form["points"] + "* ~~~~"
+            mine_sqlite3.incUser(db, session["user"][1], request.form["points"])
             db.commit()
+            session["user"] = mine_sqlite3.getUser(db, session["user"][1], session["user"][2]);
             db.close()
-        return redirect(url_for("index"))
-    if "user" in session:
-        return render_template("test.html", user=session["user"])
+            #WHAT IS THIS BULLSHIT
+        return redirect(url_for("about")) #THIS LINE WORKS
+        #return redirect(url_for("index")) #AND THIS ONE DOESN'T
+        #I WANNA GO BACK TO C
+    elif "user" in session:
+        return render_template("test.html", user=session["user"], 
+                               size=int(math.log10(session["user"][3] + 1)) + 1) #length of number
     else:
         return render_template("test.html")
 
-@app.route("/", methods = ['GET','POST'])
 @app.route("/login", methods=["GET", "POST"])
 def login():
     db = mine_sqlite3.init()
@@ -52,21 +60,20 @@ def login():
                 db.close()
                 flash("You're already a user!")
                 return redirect(url_for('login'))
+            session["user"] = mine_sqlite3.getUser(db, username, password)
         elif action == "Login":
             username = request.form["l_username"]
             password = request.form ["l_password"]
             session["user"] = mine_sqlite3.getUser(db, username, password)
-            user = session["user"]
-            if user == None:
+            if session["user"] == None:
                 db.close()
                 flash("That user is not registered!")
                 return redirect(url_for('login'))
-            else:
-                return render_template("home.html")
         elif action == "Logout":
             session.pop("user")
+
     if "user" in session:
-        return render_template("login.html", user=session["user"])
+            return render_template("login.html", user=session['user'])
     else:
         return render_template("login.html")
 
@@ -75,5 +82,6 @@ def not_found(e):
     return render_template("404.html")
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
     
